5b9078e1683da73c520c1ecbc889c9680403a883
KYLA BALIAO <108627002+kawaii-kai@users.noreply.github.com>
2026-07-28 23:13:21 +0800

    feat(core): add styles manager utility

---
+++ lib/core/theme/styles_manager.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/core/theme/font_manager.dart';
+
+TextStyle _getTextStyle(
+  double fontSize,
+  String fontFamily,
+  FontWeight fontWeight,
+  Color color,
+) {
+  return TextStyle(fontSize: fontSize, fontFamily: fontFamily, color: color);
+}
+
+//Regular Style
+TextStyle getRegularStyle({required fontSize, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontManager.fontFamily,
+    FontWeightManager.regular,
+    color,
+  );
+}
+
+//Light TextStyle
+TextStyle getLightStyle({required fontSize, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontManager.fontFamily,
+    FontWeightManager.light,
+    color,
+  );
+}
+
+//Light Italic Style
+TextStyle getItalicStyle({required fontSize, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontManager.fontFamily,
+    FontWeightManager.lightItalic,
+    color,
+  );
+}
+
+//Bold TextStyle
+TextStyle getBoldStyle({required fontSize, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontManager.fontFamily,
+    FontWeightManager.bold,
+    color,
+  );
+}
+
+//Semi-Bold TextStyle
+TextStyle getSemiBoldStyle({required fontSize, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontManager.fontFamily,
+    FontWeightManager.semiBold,
+    color,
+  );
+}
+
+//Medium TextStyle
+TextStyle getMediumStyle({required fontSize, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontManager.fontFamily,
+    FontWeightManager.medium,
+    color,
+  );
+}
