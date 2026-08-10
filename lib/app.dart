c3ab6582f6d9eea57f6d6c296e94032cd6fd25cd
KYLA BALIAO <108627002+kawaii-kai@users.noreply.github.com>
2026-08-10 22:56:17 +0800

    feat(refactor): add routes

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:my_portfolio/core/theme/routes_manager.dart';
+import 'package:my_portfolio/config/routes/routes_manager.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-            routerConfig: appRouter,
-            title: 'Okane Bank',
-            debugShowCheckedModeBanner: false,
-          );
+      title: 'Developer Portfolio',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        useMaterial3: true,
+        fontFamily: 'Inter', 
+      ),
+      routerConfig: AppRouter.router,
+    );
   }
 }
