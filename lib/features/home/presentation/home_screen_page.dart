86cceadd6759d18946ae6ff23e89aeb3b64be652
KYLA BALIAO <108627002+kawaii-kai@users.noreply.github.com>
2026-08-11 23:25:37 +0800

    feat(home): updated home screen page

--- lib/features/home/presentation/home_screen_page.dart
+++ lib/features/home/presentation/home_screen_page.dart
@@ -1,8 +1,6 @@
-import 'package:device_frame/device_frame.dart';
+
 import 'package:flutter/material.dart';
-import 'package:my_portfolio/core/presentation/layout/responsive_screen_shell.dart';
-import 'package:my_portfolio/config/theme/font_manager.dart';
-import 'package:my_portfolio/features/home/presentation/pages/home_hero_header.dart';
+import 'package:my_portfolio/features/home/presentation/widgets/responsive_screen_shell.dart';
 import 'package:my_portfolio/features/home/presentation/pages/social_service_dashboard.dart';
 
 class HomeScreenPage extends StatefulWidget {
@@ -21,13 +19,8 @@ class _HomeScreenPageState extends State<HomeScreenPage> {
         TextButton(onPressed: () {}, child: const Text('Skills')),
         TextButton(onPressed: () {}, child: const Text('Projects')),
       ],
-      // Just pass your widgets here!
       children: [
-        HomeHeroHeader(),
-
         SizedBox(height: 20),
-
-        // 2. Bottom Section: Social Media Device Dashboard
         Center(child: SocialDeviceDashboard()),
 
         SizedBox(height: 60),
