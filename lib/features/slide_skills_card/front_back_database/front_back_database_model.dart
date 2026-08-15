feb10a4c957d8a79c2a0cbb98bf981df20841c46
KYLA BALIAO <108627002+kawaii-kai@users.noreply.github.com>
2026-08-15 23:47:37 +0800

    feat(technical-skills): add front-end, back-end, database model

---
+++ lib/features/slide_skills_card/front_back_database/front_back_database_model.dart
@@ -0,0 +1,51 @@
+class FrontEndModel {
+  final List<Map<String, String>> skills = [
+    {
+      'name': 'HTML',
+      'level': 'Proficient',
+      'icon': 'assets/images/html5-original.svg',
+    },
+    {
+      'name': 'CSS',
+      'level': 'Proficient',
+      'icon': 'assets/images/css3-original.svg',
+    },
+    {
+      'name': 'JavaScript',
+      'level': 'Intermediate',
+      'icon': 'assets/images/javascript-plain.svg',
+    },
+    {
+      'name': 'React',
+      'level': 'Beginner',
+      'icon': 'assets/images/react-original.svg',
+    },
+    {
+      'name': 'npm',
+      'level': 'Beginner',
+      'icon': 'assets/images/npm-original.svg',
+    },
+
+    {
+      'name': 'Bootstrap',
+      'level': 'Intermediate',
+      'icon': 'assets/images/bootstrap-original.svg',
+    },
+    {
+      'name': 'Tailwind CSS',
+      'level': 'Beginner',
+      'icon': 'assets/images/tailwindcss-original.svg',
+    },
+     {
+      'name': 'MySQL',
+      'level': 'Intermediate',
+      'icon': 'assets/images/mysql-original.svg',
+    },
+
+    {
+      'name': 'SQL',
+      'level': 'Intermediate',
+      'icon': 'assets/images/sql.svg',
+    },
+  ];
+}
