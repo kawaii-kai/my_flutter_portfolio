ec3574a416fadf4c6e8eee772f958462b910cec8
KYLA BALIAO <108627002+kawaii-kai@users.noreply.github.com>
2026-08-15 23:51:02 +0800

    theme(technical-skills): add front-end, back-end, database card

---
+++ lib/features/slide_skills_card/front_back_database/front_back_database_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/features/slide_skills_card/front_back_database/front_back_database_model.dart';
+import 'package:my_portfolio/features/slide_skills_card/hover_style.dart';
+
+class FrontBackDatabaseCard extends StatelessWidget {
+  final double maxWidth;
+  final bool isMobile;
+  const FrontBackDatabaseCard({
+    super.key,
+    required this.maxWidth,
+    required this.isMobile,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    late FrontBackDatabaseModel skillsModel = FrontBackDatabaseModel();
+    int crossAxisCount = isMobile ? 3 : 7;
+
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossAxisCount,
+        crossAxisSpacing: isMobile ? 12 : 16,
+        mainAxisSpacing: isMobile ? 12 : 16,
+        childAspectRatio: isMobile ? 1.0 : 1.1,
+      ),
+      itemCount: skillsModel.skills.length,
+      itemBuilder: (context, index) {
+        final skill = skillsModel.skills[index];
+        final bool isSelected = index == false; // Example active item
+        // final Color levelColor = _getLevelColor(skill['level']!);
+
+        return HoverStyle(
+          skill: skill,
+          isSelected: isSelected,
+          isMobile: isMobile,
+          // levelColor: levelColor,
+        );
+      },
+    );
+  }
+}
